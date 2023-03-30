295ff3788753b94deb3da952e16751cc13a02df8
macky <macky@DESKTOP-CENLHJ3>
2023-03-30 21:44:23 +0800

    Add project files.

---
+++ ClockAngle/Program.cs
@@ -0,0 +1,64 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+       
+        Console.WriteLine("Calculate the Angle of an Analog Clock!");
+
+        Console.WriteLine("Enter Hour:  ");
+        int Hour_Hand = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Minutes:  ");
+        int Minutes_Hand = Convert.ToInt32(Console.ReadLine());
+
+
+        if (Hour_Hand >= 12)
+        {
+            Console.WriteLine("INVALID TIME! Hour should be less than 12");
+
+        }
+        else if (Minutes_Hand >= 60)
+        {
+
+            Console.WriteLine("INVALID TIME! Minutes should be less than 60");
+        }
+
+
+
+
+        else
+        {
+
+            double angle = (30 * Hour_Hand) - (5.5 * Minutes_Hand);
+
+            if (angle < 0)
+            {
+                double NewAngle = angle * -1;
+                double angle2 = 360 - NewAngle;
+
+                Console.WriteLine("Angle from hour hand is " + NewAngle);
+                Console.WriteLine("Angle from Minutes hand is " + angle2);
+
+                Console.WriteLine("The lesser ANGLE in Degrees is " + NewAngle + " degrees.");
+
+            }
+            else
+            {
+                double angle3 = 360 - angle;
+
+                Console.WriteLine("Angle from hour hand is " + angle);
+                Console.WriteLine("Angle from Minutes hand is " + angle3);
+
+                
+                Console.WriteLine("The lesser ANGLE in Degrees is " + angle + " degrees.");
+
+
+            }
+
+
+
+        }
+
+
+    }
+}
